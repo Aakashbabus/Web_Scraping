7f8ac0431cabc1970fa2d013e33e0fabdfbed380
Aakash Suresh <aakashbabus@gmail.com>
2024-05-25 09:53:05 +0530

    Made the code modular using functions

--- 02_Check_Gold_n_Silver_Price.py
+++ 02_Check_Gold_n_Silver_Price.py
@@ -1,47 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the webpage to scrape
-url = "https://upstox.com/gold-rates/gold-rates-in-bangalore/"  # Replace with the actual URL
+def main():
+    get_gold_price_India()
+    get_silver_price_India()
 
-# Send a GET request to the webpage
-response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Create a BeautifulSoup object
-    soup = BeautifulSoup(response.content, 'html.parser')
+def get_gold_price_India():
+    # URL of the webpage to scrape
+    url = "https://upstox.com/gold-rates/gold-rates-in-bangalore/"  # Replace with the actual URL
+
+    # Send a GET request to the webpage
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Create a BeautifulSoup object
+        soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Find content with specific class and extract their text
-    content = soup.find_all(class_='whitespace-nowrap')
+        # Find content with specific class and extract their text
+        content = soup.find_all(class_='whitespace-nowrap')
     
-    # Loop through each heading and print its text
-    #for heading in content:
-    #   print(heading.text)
-    print("Price of 24 Carrot Gold per gram in Bangalore today is :",content[4].text)
+        print("Price of 24 Carrot Gold per gram in Bangalore today is :",content[4].text)
 
-else:
-    print("Failed to retrieve the webpage.",response.status_code)
+    else:
+        print("Failed to retrieve the webpage.",response.status_code)
 
+def get_silver_price_India():
 
-# URL of the webpage to scrape
-url = "https://www.urbanmoney.com/silver-rate/bangalore"  # Replace with the actual URL
+    # URL of the webpage to scrape
+    url = "https://www.urbanmoney.com/silver-rate/bangalore"  # Replace with the actual URL
 
-# Send a GET request to the webpage
-response = requests.get(url)
+    # Send a GET request to the webpage
+    response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Create a BeautifulSoup object
-    soup = BeautifulSoup(response.content, 'html.parser')
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Create a BeautifulSoup object
+        soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Find content with specific class and extract their text
-    content = soup.find_all(class_='font22 textBlack fontBold mb10')
+        # Find content with specific class and extract their text
+        content = soup.find_all(class_='font22 textBlack fontBold mb10')
     
-    # Loop through each heading and print its text
-    #for heading in content:
-    #   print(heading.text)
-    print("Price of 1kg pure silver in Bangalore today is :",content[0].text)
+        # Loop through each heading and print its text
+        #for heading in content:
+        #   print(heading.text)
+        print("Price of 1kg pure silver in Bangalore today is :",content[0].text)
 
-else:
-    print("Failed to retrieve the webpage.",response.status_code)
+    else:
+        print("Failed to retrieve the webpage.",response.status_code)
+
+
+if __name__ == "__main__":
+    main()
