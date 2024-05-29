12b62dbd9c995db9ae6a874074ee4e9956350c72
Aakash Suresh <aakashbabus@gmail.com>
2024-05-29 19:06:06 +0530

    Output in tabular form created , international gold price added

--- 02_Check_Gold_n_Silver_Price.py
+++ 02_Check_Gold_n_Silver_Price.py
@@ -1,55 +1,101 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from tabulate import tabulate
+import json
 
 def main():
-    get_gold_price_India()
-    get_silver_price_India()
+    gold_prices = get_gold_price_India()
+    silver_prices = get_silver_price_India()
+    
+    print("\nGold and Silver Prices in Bangalore :\n")
+    # Combine the data into a single DataFrame
+    data = {
+        'Period': ["Today", "30 Days", "180 Days"],
+        'Gold /10g': gold_prices,
+        'Silver /1Kg': silver_prices
+    }
+    df = pd.DataFrame(data)
+    
+    # Display the DataFrame as a table with borders
+    print(tabulate(df, headers='keys', tablefmt='grid'))
+    print("\n")
 
+    make_gapi_request()
+    print("\n")
 
 def get_gold_price_India():
-    # URL of the webpage to scrape
     url = "https://upstox.com/gold-rates/gold-rates-in-bangalore/"  # Replace with the actual URL
-
-    # Send a GET request to the webpage
     response = requests.get(url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Create a BeautifulSoup object
         soup = BeautifulSoup(response.content, 'html.parser')
-    
-        # Find content with specific class and extract their text
-        content = soup.find_all(class_='whitespace-nowrap')
-    
-        print("Price of 24 Carrot Gold per gram in Bangalore today is :",content[4].text)
+        gold_prices = []
 
+        content = soup.find_all(class_='whitespace-nowrap')
+        gold_prices.append(content[4].text)
+        
+        row = soup.find_all('tr')[19]
+        column1 = row.find_all('td')[1].text
+        gold_prices.append(column1)
+
+        row = soup.find_all('tr')[22]
+        column1 = row.find_all('td')[1].text
+        gold_prices.append(column1)
+
+        return gold_prices
     else:
-        print("Failed to retrieve the webpage.",response.status_code)
+        print("Failed to retrieve the webpage.", response.status_code)
+        return []
 
 def get_silver_price_India():
-
-    # URL of the webpage to scrape
     url = "https://www.urbanmoney.com/silver-rate/bangalore"  # Replace with the actual URL
-
-    # Send a GET request to the webpage
     response = requests.get(url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Create a BeautifulSoup object
         soup = BeautifulSoup(response.content, 'html.parser')
-    
-        # Find content with specific class and extract their text
+        silver_prices = []
+
         content = soup.find_all(class_='font22 textBlack fontBold mb10')
-    
-        # Loop through each heading and print its text
-        #for heading in content:
-        #   print(heading.text)
-        print("Price of 1kg pure silver in Bangalore today is :",content[0].text)
+        silver_prices.append(content[0].text)
+        silver_prices.append("TBD")
+        silver_prices.append("TBD")
 
+        return silver_prices
     else:
-        print("Failed to retrieve the webpage.",response.status_code)
+        print("Failed to retrieve the webpage.", response.status_code)
+        return []
+    
+def make_gapi_request():
+    api_key = "YOUR API KEY"
+    symbol = "XAU"
+    curr = "INR"
+    date = ""
 
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}{date}"
+    
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        result = response.json()  # Parse response text to JSON
+        price_gram_24k = result.get("price_gram_24k")  # Safely extract the value
+        if price_gram_24k is not None:
+            print(" International Gold price per gram: ₹",price_gram_24k)
+        else:
+            print("price_gram_24k not found in the response")
+        
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
 
 if __name__ == "__main__":
     main()
+
+
+
+
